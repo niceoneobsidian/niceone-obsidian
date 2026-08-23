29f96702a212d9bb92da18c0a76262b0c72ccca5
Niceone Obsidian <niceoneobsidian@gmail.com>
2026-08-22 23:19:01 -0400

    fix: sort registry exports for ruff

--- ois/registries/__init__.py
+++ ois/registries/__init__.py
@@ -1,10 +1,10 @@
 """First-class OIS registries."""
 
+from .agent_registry import AgentRegistry
 from .base import Registry, RegistryEntry
 from .capability_registry import CapabilityRegistry
-from .agent_registry import AgentRegistry
-from .tool_registry import ToolRegistry
 from .model_registry import ModelRegistry
+from .tool_registry import ToolRegistry
 from .workflow_registry import WorkflowRegistry
 
 __all__ = [
