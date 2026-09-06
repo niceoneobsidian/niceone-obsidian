608819a1b26069045e4a8cf8b328c2f2c5792956
Niceone Obsidian <niceoneobsidian@gmail.com>
2026-09-06 16:28:54 -0700

    feat(evolution): expose controlled evolution boundary

--- ois/evolution/__init__.py
+++ ois/evolution/__init__.py
@@ -1,5 +1,12 @@
 """Controlled evolution contracts."""
 
+from .controlled import ControlledEvolution, EvolutionProposal, EvolutionState
 from .engine import EvolutionCandidate, EvolutionPlane
 
-__all__ = ["EvolutionCandidate", "EvolutionPlane"]
+__all__ = [
+    "ControlledEvolution",
+    "EvolutionCandidate",
+    "EvolutionPlane",
+    "EvolutionProposal",
+    "EvolutionState",
+]
