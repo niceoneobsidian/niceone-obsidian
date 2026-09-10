6c514c83223bf5f43b6c96f4bc5db8d8e82c6c46
Obsidian Architect <obsidian.intelligence.hq@gmail.com>
2026-09-10 04:36:04 -0400

    feat: export canonical market catalog

--- ois/domains/football_market_intelligence/__init__.py
+++ ois/domains/football_market_intelligence/__init__.py
@@ -7,35 +7,22 @@ by OIS platform services.
 
 from .evaluation import evaluate_market_event
 from .integration import translate_market
+from .market_catalog import MarketDefinition, MarketFamily, catalog, normalize_market_key, provider_market_mapping
 from .markets import MarketType, Selection
 from .schemas import MarketEvent, MarketOutcome
 from .settlement import settle_market
-from .web_adapters import (
-    FOOTBALL_DATA,
-    ODDS_API,
-    SPORTMONKS,
-    fetch_and_parse,
-    parse_football_data_matches,
-    parse_odds_api,
-    parse_sportmonks_odds,
-)
+from .web_adapters import FOOTBALL_DATA, ODDS_API, SPORTMONKS, fetch_and_parse, parse_football_data_matches, parse_odds_api, parse_sportmonks_odds
 from .web_normalization import assert_temporal_integrity, check_provider_health, normalize_observations
-from .web_sources import (
-    MatchObservation,
-    NormalizationResult,
-    OddsObservation,
-    ProviderHealth,
-    WebObservation,
-    WebSource,
-    WebTransport,
-)
+from .web_sources import MatchObservation, NormalizationResult, OddsObservation, ProviderHealth, WebObservation, WebSource, WebTransport
 
 __all__ = [
     "FOOTBALL_DATA",
     "ODDS_API",
     "SPORTMONKS",
     "MatchObservation",
+    "MarketDefinition",
     "MarketEvent",
+    "MarketFamily",
     "MarketOutcome",
     "MarketType",
     "NormalizationResult",
@@ -46,13 +33,16 @@ __all__ = [
     "WebSource",
     "WebTransport",
     "assert_temporal_integrity",
+    "catalog",
     "check_provider_health",
     "evaluate_market_event",
     "fetch_and_parse",
+    "normalize_market_key",
     "normalize_observations",
     "parse_football_data_matches",
     "parse_odds_api",
     "parse_sportmonks_odds",
+    "provider_market_mapping",
     "settle_market",
     "translate_market",
 ]
