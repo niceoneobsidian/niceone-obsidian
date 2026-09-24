435123548c878e3255c8fdf075857311d0a7bd63
Niceone Obsidian <niceoneobsidian@gmail.com>
2026-09-24 09:55:03 -0700

    style: format migration helper query

--- tests/helpers/migrations.py
+++ tests/helpers/migrations.py
@@ -51,7 +51,10 @@ def apply_migrations(
             """
         )
 
-        cursor.execute("SELECT version, checksum FROM schema_migrations ORDER BY CAST(version AS INTEGER)")
+        cursor.execute(
+            "SELECT version, checksum FROM schema_migrations "
+            "ORDER BY CAST(version AS INTEGER)"
+        )
         applied_migrations = {row[0]: row[1] for row in cursor.fetchall()}
 
         for migration_file in migration_files:
