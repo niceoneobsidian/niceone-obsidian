06e9399e5c2ef7847c2e23e854df6cedac10c7cc
Niceone Obsidian <niceoneobsidian@gmail.com>
2026-09-25 13:33:24 -0700

    style: format OIS exports and tests

--- production/__init__.py
+++ production/__init__.py
@@ -11,7 +11,13 @@ from production.control_plane import (
 )
 from production.evolution import CanaryController, LearningLoop, Measurement
 from production.knowledge_world import KnowledgeWorldStore, WorldEntity, WorldFact
-from production.model_gateway import ModelGateway, ModelGatewayError, ModelRequest, ModelResponse, ModelSpec
+from production.model_gateway import (
+    ModelGateway,
+    ModelGatewayError,
+    ModelRequest,
+    ModelResponse,
+    ModelSpec,
+)
 from production.production_loop import LoopArtifact, ProductionLoopCertificate, ProductionLoopProof
 from production.semantic_world import SemanticWorld
 from production.workers import LeaseQueue, Worker
